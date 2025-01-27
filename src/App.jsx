6ad8d2127b88f99b43ae4aaf92fce82c4fb4c3e4
rayray39 <raynerlimfy@gmail.com>
2025-01-27 15:59:38 +0800

    Add FillBio to routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import CreateAccount from './CreateAccount'
 import Login from './Login'
 import WelcomePage from './WelcomePage';
+import FillBio from './FillBio';
 
 function App() {
     return <>
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<WelcomePage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/create-account" element={<CreateAccount />} />
+                <Route path="/fill-in-bio" element={<FillBio />} />
             </Routes>
         </Router>
     </>
