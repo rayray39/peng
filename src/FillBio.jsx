d8590a6bb3a5437045550fc5c25dc7c9b4759b29
rayray39 <raynerlimfy@gmail.com>
2025-01-27 15:34:03 +0800

    Create FillBio.jsx for user to fill in biography

---
+++ src/FillBio.jsx
@@ -0,0 +1,19 @@
+import { Box, TextField, Stack } from "@mui/material"
+
+function FillBio() {
+    return (
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            transform: "translate(0%, 50%)", // Center it perfectly
+        }}>
+            <Stack>
+                <h2>Write a short paragraph about yourself.</h2>
+
+                <TextField id="bio-textfield-multiline" label="Write a bio." multiline maxRows={4}/>
+            </Stack>
+        </Box>
+    )
+}
+
+export default FillBio
