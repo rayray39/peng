f2e96a47cbf034d93b52415e61dda87b3535ca38
rayray39 <raynerlimfy@gmail.com>
2025-01-27 23:17:55 +0800

    Add Hobbies page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CreateAccount from './CreateAccount'
 import Login from './Login'
 import WelcomePage from './WelcomePage';
 import FillBio from './FillBio';
+import Hobbies from './Hobbies';
 
 function App() {
     return <>
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/create-account" element={<CreateAccount />} />
                 <Route path="/fill-in-bio" element={<FillBio />} />
+                <Route path='/select-hobbies' element={<Hobbies/>}/>
             </Routes>
         </Router>
     </>
---
+++ src/Hobbies.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+function Hobbies() {
+    return (
+        <Box>
+            <h2>This is the hobbies page</h2>
+        </Box>
+    )
+}
+
+export default Hobbies
