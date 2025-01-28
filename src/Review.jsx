53038cd794559fb558fd26d8397395a9f69e10ff
rayray39 <raynerlimfy@gmail.com>
2025-01-28 23:49:49 +0800

    Add review cards to Review page

--- src/Review.jsx
+++ src/Review.jsx
@@ -1,24 +1,32 @@
 import { Box, Card, CardContent, Typography } from "@mui/material";
 
 function Review() {
+    const reviews = [
+        "Found the love of my life through Peng.",
+        "Simple to use, straightforward and no nonsense.",
+        "The people here are really nice and friendly."
+    ]
+
     return (
         <Box sx={{
             position: 'absolute',
             bottom: 0, // Anchors to the bottom
-            width: '500px', // Optional: Make it full-width
+            width: '500px',
             textAlign: 'center', // Center content horizontally
         }}>
-            <Card variant="outlined">
-            <CardContent>
-                <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 16 }}>
-                    Review
-                </Typography>
-                
-                <Typography variant="body2">
-                    review body
-                </Typography>
-            </CardContent>
-            </Card>
+            {reviews.map((review) => (
+                <Card variant="outlined" key={review}>
+                <CardContent>
+                    <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 16 }}>
+                        Review
+                    </Typography>
+                    
+                    <Typography variant="body2">
+                        {review}
+                    </Typography>
+                </CardContent>
+                </Card>
+            ))}
         </Box>
     )
 }
