53345ceffaf3e1787945827eb4870611c39b8d40
rayray39 <raynerlimfy@gmail.com>
2025-01-28 15:48:00 +0800

    Add hobbies selecttion using MUI Chip

--- src/Hobbies.jsx
+++ src/Hobbies.jsx
@@ -1,14 +1,56 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, TextField, Chip } from "@mui/material";
+import { useState } from "react";
 
 function Hobbies() {
+    const availableHobbies = [
+        "Reading", "Travelling", "Cooking",
+        "Gaming", "Music", "Sports",
+        "Films/Shows", "Outdoors", "Fitness",
+        "Handicrafts", "Food"
+    ];
+    const [selectedHobbies, setSelectedHobbies] = useState([]);
+
+    const handleChipClick = (hobby) => {
+        // when a single chip is being clicked on
+
+        if (selectedHobbies.includes(hobby)) {
+            // Remove hobby from selected hobbies
+            setSelectedHobbies(selectedHobbies.filter((item) => item !== hobby));
+        } else {
+            // Add hobby to selected hobbies
+            setSelectedHobbies([...selectedHobbies, hobby]);
+        }
+    }
+
     return (
         <Box sx={{
             display: 'flex',
             justifyContent: 'center',
             transform: "translate(0%, 50%)", // Center it perfectly
         }}>
-            <Stack>
+            <Stack direction='column' spacing={2} sx={{width: '500px'}}>
                 <h2>Select 3 hobbies 🏸🍣🛫</h2>
+
+                <TextField label='Selected Hobbies' value={selectedHobbies.join(", ")} variant="outlined"
+                    slotProps={{
+                        input: {
+                            readOnly: true,
+                        },
+                    }}
+                />
+
+                {/* Chips for selecting hobbies */}
+                <Box display="flex" flexWrap="wrap" gap={1} sx={{justifyContent: 'center', width:'500px'}}>
+                    {availableHobbies.map((hobby) => (
+                    <Chip
+                        key={hobby}
+                        label={hobby}
+                        onClick={() => handleChipClick(hobby)}
+                        color={selectedHobbies.includes(hobby) ? "primary" : "default"}
+                        variant={selectedHobbies.includes(hobby) ? "filled" : "outlined"}
+                    />
+                    ))}
+                </Box>
             </Stack>
         </Box>
     )
