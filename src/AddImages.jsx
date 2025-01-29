548fd6dcd9738904aa617fa68e2e2dc3754f0c7e
rayray39 <raynerlimfy@gmail.com>
2025-01-29 23:37:44 +0800

    AddImages.jsx: Add button to upload images

--- src/AddImages.jsx
+++ src/AddImages.jsx
@@ -1,6 +1,26 @@
-import { Box, Stack } from "@mui/material"
+import { Box, Stack, Button } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import { useState } from "react";
 
 function AddImages() {
+    const [images, setImages] = useState([]);
+
+    const handleNext = () => {
+        console.log('next button is clicked');
+    }
+
+    const VisuallyHiddenInput = styled('input')({
+        clip: 'rect(0 0 0 0)',
+        clipPath: 'inset(50%)',
+        height: 1,
+        overflow: 'hidden',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        whiteSpace: 'nowrap',
+        width: 1,
+    });
+
     return (
         <Box sx={{
             display: 'flex',
@@ -9,6 +29,28 @@ function AddImages() {
         }}>
             <Stack direction='column' spacing={2} sx={{width: '500px'}}>
                 <h2>Add at least 1 image of yourself 📹</h2>
+
+                <Button
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    disableElevation
+                    sx={{
+                        backgroundColor: 'orange'
+                    }}
+                >
+                    Select Image
+                    <VisuallyHiddenInput
+                        type="file"
+                        onChange={(event) => console.log(event.target.files)}
+                        multiple
+                    />
+                </Button>
+
+                <Button variant="contained" sx={{height:'50px', backgroundColor:'orange', marginTop:'20px'}} 
+                    disableElevation onClick={handleNext} >Next
+                </Button>
             </Stack>
         </Box>
     )
