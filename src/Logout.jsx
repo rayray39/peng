45475b0d289f8fd8029b7c5e21df7c3d43fd38b0
rayray39 <raynerlimfy@gmail.com>
2025-02-14 21:43:45 +0800

    Add logout page

---
+++ src/Logout.jsx
@@ -0,0 +1,26 @@
+import { Box, Button, Stack } from "@mui/material"
+
+function Logout() {
+
+    const handleReturnHome = () => {
+
+    }
+
+    return (
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            transform: "translate(0%, 50%)", // Center it perfectly
+        }}>
+            <Stack>
+                <h2>You've been logged out. 🚪</h2>
+
+                <Button variant="contained" sx={{height:'50px', backgroundColor:'orange', marginTop:'20px'}} 
+                    disableElevation onClick={handleReturnHome}>return back
+                </Button>
+            </Stack>
+        </Box>
+    )
+}
+
+export default Logout
