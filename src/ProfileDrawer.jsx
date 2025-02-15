264c2572494193bf7f6a8262cbee0d727eacf55c
rayray39 <raynerlimfy@gmail.com>
2025-02-15 23:22:21 +0800

    Add custom component to style each user info

--- src/ProfileDrawer.jsx
+++ src/ProfileDrawer.jsx
@@ -1,20 +1,68 @@
-import { Box, ListItem, ListItemText, Stack } from "@mui/material"
+import { Box, Chip, ListItem, ListItemText, Stack, Divider } from "@mui/material"
 
 // drawer that will be opened in People, to display profile of currently logged in user
 function ProfileDrawer({ user }) {
-    return <Box sx={{width:'240px', padding:'20px'}}>
+
+    const hobbyChips = (hobbiesString) => {
+        // generate the chips for each hobby of the user
+        const hobbies = hobbiesString.split(',');
+        return hobbies.map((hobby, index) => (
+            <Chip 
+                key={index}
+                label={hobby}
+                variant="filled"
+                sx={{
+                    backgroundColor: 'orange',
+                    color: 'white',
+                }}
+            />
+        ))
+    }
+
+    const userFirstAndLastName = (firstName, lastName) => {
+        // styles the user's first and last names
+        return (
+            `${firstName[0].toUpperCase() + firstName.slice(1)} 
+            ${lastName[0].toUpperCase() + lastName.slice(1)}`
+        )
+    }
+
+    return <Box sx={{width:'280px', padding:'10px'}}>
         <ListItem disablePadding>
             <Stack>
-                <ListItemText primary={'Profile'} />
-                <ListItemText primary={`
-                    ${user.firstName[0].toUpperCase() + user.firstName.slice(1)} 
-                    ${user.lastName[0].toUpperCase() + user.lastName.slice(1)}`} />
-                <ListItemText primary={`username: ${user.username}`} />
-                <ListItemText primary={`bio: ${user.bio}`} />
-                <ListItemText primary={`hobbies: ${user.hobbies}`} />
+                <ProfileSection sectionTitle={'PROFILE'} 
+                    sectionContent={userFirstAndLastName(user.firstName, user.lastName)}
+                />
+
+                <ProfileSection sectionTitle={'USERNAME'} sectionContent={user.username} />
+
+                <ProfileSection sectionTitle={'BIO'} sectionContent={user.bio} />
+                
+                <ProfileSection sectionTitle={'HOBBIES'} 
+                    sectionContent={
+                        <Stack direction={'row'} spacing={1}>
+                            {hobbyChips(user.hobbies)}
+                        </Stack>
+                    }
+                />
             </Stack>
         </ListItem>
     </Box>
+}
+
+// returns a component for each category of user information
+function ProfileSection({ sectionTitle, sectionContent }) {
+    return (
+        <Stack>
+            <h3 style={{color:'orange'}}>{sectionTitle}</h3>
+
+            <Box>
+                {sectionContent}
+            </Box>
+
+            <Divider sx={{marginTop:'5px'}}/>
+        </Stack>
+    )
 }
 
 export default ProfileDrawer
