900765d4d71cb2f0d3193b2d4d8cec18abcf879b
rayray39 <raynerlimfy@gmail.com>
2025-02-15 17:09:58 +0800

    Add all user info provided by logged in user

--- src/ProfileDrawer.jsx
+++ src/ProfileDrawer.jsx
@@ -1,12 +1,17 @@
-import { Box, ListItem, ListItemButton, ListItemText } from "@mui/material"
+import { Box, ListItem, ListItemText, Stack } from "@mui/material"
 
 // drawer that will be opened in People, to display profile of currently logged in user
 function ProfileDrawer({ user }) {
-    return <Box sx={{width:'200px'}}>
+    return <Box sx={{width:'240px', padding:'20px'}}>
         <ListItem disablePadding>
-            <ListItemText primary={'Profile'} />
-            <ListItemText primary={user.username} />
-          </ListItem>
+            <Stack>
+                <ListItemText primary={'Profile'} />
+                <ListItemText primary={`${user.firstName} ${user.lastName}`} />
+                <ListItemText primary={`username: ${user.username}`} />
+                <ListItemText primary={`bio: ${user.bio}`} />
+                <ListItemText primary={`hobbies: ${user.hobbies}`} />
+            </Stack>
+        </ListItem>
     </Box>
 }
 
