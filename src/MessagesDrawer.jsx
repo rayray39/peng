449d514d6925672e051b5a27cd2b5586f19a9091
rayray39 <raynerlimfy@gmail.com>
2025-02-17 10:09:31 +0800

    Update styling of messages drawer

--- src/MessagesDrawer.jsx
+++ src/MessagesDrawer.jsx
@@ -4,7 +4,7 @@ function MessagesDrawer() {
     return <Box sx={{width:'280px', padding:'10px'}}>
         <ListItem disablePadding>
             <Stack>
-                <h2>MESSAGES</h2>
+                <h3 style={{color:'orange'}}>MESSAGES</h3>
             </Stack>
         </ListItem>
     </Box>
