e79df93eb37771229d8ee8889bd1bf8ff53efda7
rayray39 <raynerlimfy@gmail.com>
2025-02-18 19:07:59 +0800

    Add content to messages and styling

--- src/Messages.jsx
+++ src/Messages.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material"
+import { Box, Stack } from "@mui/material"
 import { useParams } from "react-router-dom"
 
 // opens the messages currently logged in user has with username
@@ -6,9 +6,17 @@ function Messages() {
     const { username } = useParams();
 
     return (
-        <Box>
+        <Stack direction={"column"} sx={{
+            border:'1px solid black', 
+            width:'500px',
+            margin:'auto'
+        }}>
             <h2>{`This is the message with ${username}`}</h2>
-        </Box>
+
+            <Stack sx={{border:'1px solid black'}}>
+                <h1>some other message</h1>
+            </Stack>
+        </Stack>
     )
 }
 
