34947973826e1c6187cc02f0aa8dc818a8afcf4b
rayray39 <raynerlimfy@gmail.com>
2025-02-18 19:40:45 +0800

    Add send button and textfield

--- src/Messages.jsx
+++ src/Messages.jsx
@@ -1,21 +1,45 @@
-import { Box, Stack } from "@mui/material"
+import { Box, Stack, Button, TextField } from "@mui/material"
 import { useParams } from "react-router-dom"
+import { useUser } from "./UserContext";
 
 // opens the messages currently logged in user has with username
 function Messages() {
     const { username } = useParams();
+    const { currentUser } = useUser();
+
+    const fetchMessages = async () => {
+        console.log('fetching messages');
+    }
+
+    const sendMessage = async () => {
+        console.log('sending message');
+    }
 
     return (
         <Stack direction={"column"} sx={{
-            border:'1px solid black', 
+            // border:'1px solid black', 
             width:'500px',
             margin:'auto'
         }}>
-            <h2>{`This is the message with ${username}`}</h2>
+            <h3>{`${username} ❣️`}</h3>
 
-            <Stack sx={{border:'1px solid black'}}>
-                <h1>some other message</h1>
+            <Stack sx={{border:'1px solid black', height:'300px'}}>
+                message content
             </Stack>
+
+            <TextField variant="filled" sx={{
+                
+            }}/>
+            <Button onClick={sendMessage} variant="outlined" disableElevation
+                sx={{
+                    color:'white',
+                    backgroundColor:'orange',
+                    borderColor:'orange',
+                    marginTop:'10px'
+                }}
+            >
+                Send
+            </Button>
         </Stack>
     )
 }
