e98adb5f0f9bb916bf0c86cb96f2e8ac4fe1fcda
rayray39 <raynerlimfy@gmail.com>
2025-02-20 17:22:45 +0800

    Messages.jsx: Display new message sent

--- src/Messages.jsx
+++ src/Messages.jsx
@@ -1,18 +1,77 @@
 import { Box, Stack, Button, TextField } from "@mui/material"
 import { useParams } from "react-router-dom"
 import { useUser } from "./UserContext";
+import { useState } from "react";
 
 // opens the messages currently logged in user has with username
 function Messages() {
     const { username } = useParams();
     const { currentUser } = useUser();
 
+    const [messages, setMessages] = useState([]);               // past messages from database
+    const [messageContent, setMessageContent] = useState('');   // newly sent message
+    const [displayMessages, setDisplayMessages] = useState([]);
+
+    const handleMessageContent = (event) => {
+        setMessageContent(event.target.value);
+    }
+
     const fetchMessages = async () => {
         console.log('fetching messages');
+
+        // const token = localStorage.getItem('authToken');
+
+        // const response = await fetch('http://localhost:5000/get-all-messages', {
+        //     method:'GET',
+        //     headers:{
+        //         'Content-Type':'application/json',
+        //         'Authorization':`Bearer ${token}`
+        //     },
+        //     body:JSON.stringify({
+        //         currentUser, username
+        //     })
+        // })
+
+        // const data = await response.json();
+
+        // if (!response.ok) {
+        //     console.log(data.error);
+        //     return;
+        // }
+
+        // console.log(data.message);
+        // setMessages(data.messages);
+        // setDisplayMessages(data.messages);
     }
 
     const sendMessage = async () => {
-        console.log('sending message');
+        // saves the sent message in the database
+        console.log(`sending message to ${username}: ${messageContent}`);
+
+        // const token = localStorage.getItem('authToken');
+
+        // const response = await fetch('http://localhost:5000/send-message', {
+        //     method:'POST',
+        //     headers:{
+        //         'Content-Type':'application/json',
+        //         'Authorization':`Bearer ${token}`
+        //     },
+        //     body:JSON.stringify({
+        //         currentUser, username, messageContent
+        //     })
+        // })
+
+        // const data = await response.json();
+
+        // if (!response.ok) {
+        //     console.log(data.error);
+        //     return;
+        // }
+
+        // console.log(data.message);
+        setDisplayMessages((prev) => [...prev, messageContent]);
+        setMessageContent('');
+        document.getElementById('message-textfield')?.blur();   // clear the textfield after message sent
     }
 
     return (
@@ -24,12 +83,19 @@ function Messages() {
             <h3>{`${username} ❣️`}</h3>
 
             <Stack sx={{border:'1px solid black', height:'300px'}}>
-                message content
+                {
+                    displayMessages ? 
+                    displayMessages.map((message, index) => (
+                        <Box key={index}>
+                            {message}
+                        </Box>
+                    )) :
+                    null
+                }
             </Stack>
 
-            <TextField variant="filled" sx={{
-                
-            }}/>
+            <TextField id="message-textfield" variant="filled" value={messageContent} onChange={handleMessageContent} />
+
             <Button onClick={sendMessage} variant="outlined" disableElevation
                 sx={{
                     color:'white',
